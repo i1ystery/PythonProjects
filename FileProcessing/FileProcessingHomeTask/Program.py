272733bf06012d37097807abc15ca3944351fe41
Max Kuzma <kuzmamax78@gmail.com>
2021-12-21 17:15:42 +0100

    FileProcessingHomeTask

--- FileProcessing/FileProcessingHomeTask/Program.py
+++ FileProcessing/FileProcessingHomeTask/Program.py
@@ -6,6 +6,7 @@ import traceback
 import xml.etree.cElementTree as ET
 from datetime import datetime
 
+__author__ = 'Maksym Kuzma C4a'
 
 default_config = {
     'Language': 'English',
@@ -14,22 +15,27 @@ default_config = {
     'Export path': './export.csv',
     'Error path': './error.log'
 }
-config_path = 'cfg.json'
-current_config = {}
-data_to_save = []
+config_path = 'cfg.json'  # Path to json config
+current_config = {}       # Used to store loaded from file json config
 
 
-def create_person(name, lastname, job, phone_number):
-    data_to_save.append([name, lastname, job, phone_number])
-
-
-def export_persons():
+def export_person(name: str, lastname: str, job: str, phone_number: int):
+    """
+    Exports input data as list into binary file
+    :param name: Person name
+    :param lastname: Person lastname
+    :param job: Person job
+    :param phone_number: Person phone number
+    """
     with open(current_config['Data path'], 'ab') as file:
-        for obj in data_to_save:
-            pickle.dump(obj, file)
+        pickle.dump([name, lastname, job, phone_number], file)
 
 
-def load_data():
+def load_data() -> list[list]:
+    """
+    Loads data from binary file
+    :return: list of persons
+    """
     with open(current_config['Data path'], 'rb') as file:
         person_list = []
         try:
@@ -41,11 +47,17 @@ def load_data():
 
 
 def create_config():
+    """
+    Creates default json config file
+    """
     with open(config_path, 'w') as file:
         json.dump(default_config, file, indent=True)
 
 
 def load_config():
+    """
+    Loads json config from file and sets current_config from loaded file
+    """
     global current_config
     if os.path.exists(config_path):
         with open(config_path, 'r') as file:
@@ -55,7 +67,15 @@ def load_config():
         current_config = default_config
 
 
-def save_changes_to_config(lang, enc, data_path, export_path, error_path):
+def save_changes_to_config(lang: str, enc: str, data_path: str, export_path: str, error_path: str):
+    """
+    Saves changes to current json config
+    :param lang: Language
+    :param enc: Encryption
+    :param data_path: Path to data.dat file
+    :param export_path: Path to export.csv file
+    :param error_path: Path to error.log file
+    """
     with open(config_path, 'w') as file:
         current_config['Language'] = lang
         current_config['Encoding'] = enc
@@ -66,13 +86,22 @@ def save_changes_to_config(lang, enc, data_path, export_path, error_path):
 
 
 def export_data_to_csv():
+    """
+    Exports data from data.dat binary file to csv file
+    """
     data = load_data()
     with open(current_config['Export path'], 'w') as file:
         writer = csv.writer(file)
         writer.writerows(data)
 
 
-def log_error(exc_type, exc_value, exc_tb):
+def log_error(exc_type: type, exc_value, exc_tb: traceback):
+    """
+    Logs exception into XML file
+    :param exc_type: Exception type from sys.exc_info()
+    :param exc_value: Exception value from sys.exc_info()
+    :param exc_tb: Traceback from sys.exc_info()
+    """
     error_time = datetime.today().__str__()
     traceback_str = ''
     for trb in traceback.format_tb(exc_tb):
@@ -89,4 +118,3 @@ def log_error(exc_type, exc_value, exc_tb):
     ET.SubElement(exc, 'Time').text = error_time
     ET.indent(tree, space="\t", level=0)
     tree.write(current_config['Error path'])
-
