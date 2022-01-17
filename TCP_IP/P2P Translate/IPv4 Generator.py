ddac667073086f352a1d04f9b65d7ab39432bc1c
Max Kuzma <kuzmamax78@gmail.com>
2022-01-17 23:55:28 +0100

    P2P Small Update

--- TCP_IP/P2P Translate/IPv4 Generator.py
+++ TCP_IP/P2P Translate/IPv4 Generator.py
@@ -22,3 +22,19 @@ print(len(a))
 #
 #
 # print(get_all_possible_addresses())
+
+
+# importing socket module
+import socket
+
+# creates a new socket using the given address family.
+socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# setting up the default timeout in seconds for new socket object
+socket.setdefaulttimeout(1)
+
+# returns 0 if connection succeeds else raises error
+result = socket_obj.connect_ex((addr, port))  # address and port in the tuple format
+
+# closes te object
+socket_obj.close()
