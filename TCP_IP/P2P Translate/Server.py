0f19c39cfb23c65a80d15dfb7513d248489b20a4
Max Kuzma <kuzmamax78@gmail.com>
2022-01-20 21:57:43 +0100

    P2P Working update

--- TCP_IP/P2P Translate/Server.py
+++ TCP_IP/P2P Translate/Server.py
@@ -1,4 +1,5 @@
 import json
+import os.path
 import socket
 import threading
 import ipaddress
@@ -6,10 +7,33 @@ from concurrent.futures import ThreadPoolExecutor
 from itertools import repeat
 
 
-def load_config():
-    with open("config.json", 'r') as f:
-        return json.load(f)
+class NotConfigured(Exception):
+    pass
 
+
+def load_config() -> dict:
+    """
+    Loads config from json file
+    :return: config as dictionary
+    """
+    if os.path.isfile('config.json'):
+        with open("config.json", 'r') as f:
+            return json.load(f)
+    else:
+        cfg = {
+            "IP": "127.0.0.1",
+            "Port": 65525,
+            "IP_RANGE": "10.2.0.0/16",
+            "PORT_RANGE": "65525-65535"
+            }
+        with open('config.json', 'w') as f:
+            json.dump(cfg, f, indent=True)
+            raise NotConfigured
+
+
+def log(message: str):
+    with open("server_log.txt", 'a') as f:
+        f.write(message)
 
 words = {
     'future': 'budoucnost',
@@ -23,6 +47,9 @@ config = load_config()
 
 
 def translate_loc(conn, word: str):
+    """
+    Finds translation for requested word in local dictionary
+    """
     if word.lower() in words.keys():
         conn.send(f'TRANSLATESUC"{words[word.lower()]}"'.encode())
     else:
@@ -30,13 +57,22 @@ def translate_loc(conn, word: str):
 
 
 def translate_rem(conn, word: str):
+    """
+    Finds translation for requested word in dictionaries from all available servers in defined IP range
+    """
     find_available_servers()
     if available_servers:
         with ThreadPoolExecutor(max_workers=len(available_servers)) as executor:
             executor.map(find_translation, available_servers, repeat(word), repeat(conn))
+    else:
+        conn.send('TRANSLATEERR"NO AVAILABLE SERVERS FOUND"')
 
 
 def translate_any(conn, word: str):
+    """
+    Finds translation for requested word in local dictionary.
+    If words wasn't found, finds translation for requested word in dictionaries from all available servers in defined IP range
+    """
     if word.lower() in words.keys():
         conn.send(f'TRANSLATESUC"{words[word.lower()]}"'.encode())
     else:
@@ -52,9 +88,12 @@ commands = {
 
 
 def find_translation(server, word: str, conn):
+    """
+    Used to connect to defined server and request translation for a word
+    """
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(3)
     client.connect(server)
-    client.recv(1024)
     client.send(f'TRANSLATELOC"{word}"'.encode())
     answer = client.recv(1024)
     print(answer)
@@ -62,50 +101,62 @@ def find_translation(server, word: str, conn):
 
 
 def find_available_servers():
-    all_servers = [str(ip) for ip in ipaddress.IPv4Network(config['IP_RANGE'])][1:-1]
+    """
+    Finds available servers in defined IP range
+    """
+    all_servers = [str(ip) for ip in ipaddress.IPv4Network(config['IP_RANGE'])][1:-1]  # All possible server IP's excluding Network address and Broadcast address
     with ThreadPoolExecutor(max_workers=len(all_servers)) as executor:
         executor.map(check_ip, all_servers)
 
 
 def check_ip(server):
+    """
+    Used for checking IP with every port from port range.
+    If server responds adds it to available server list.
+    :param server: IP
+    """
+    socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(0.01)
     try:
         if server == config['IP']:
             raise
         port_min, port_max = config['PORT_RANGE'].split('-')
         ports = range(int(port_min), int(port_max) + 1)
         for port in ports:
-            print(f'Checking {server}, {port}')
-            socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            socket.setdefaulttimeout(0.01)
             socket_obj.connect_ex((server, port))
-            socket_obj.recv(1024)
             socket_obj.send('TRANSLATELOC"asdasdasd"'.encode())
             msg = socket_obj.recv(1024).decode()
-            print(msg)
-            socket_obj.close()
             if 'TRANSLATE' in msg:
-                print(f'Found {server} + {port}')
                 available_servers.append((server, port))
-    except:
+    except TimeoutError:
         pass
+    except Exception as e:
+        import traceback
+        print(server)
+        print(traceback.format_exc())
+    finally:
+        socket_obj.close()
 
 
-def client_thread(conn, addr):
-    while True:
-        try:
+def client_thread(conn):
+    """
+    Used for handling client inputs
+    """
+    try:
+        message = conn.recv(1024).decode()
+        if message == '\r\n' or message == '\r':
             message = conn.recv(1024).decode()
-            if message == '\r\n' or message == '\r':
-                message = conn.recv(1024).decode()
-            command, word = message.split('"')[:-1]
-            if command.upper() in commands.keys():
-                commands[command.upper()](conn, word)
-            else:
-                raise ValueError
-        except ValueError:
-            conn.send('TRASNLATEERR"Invalid Command"'.encode())
-            conn.close()
-        except:
-            conn.close()
+        command, word = message.split('"')[:-1]
+        if command.upper() in commands.keys():
+            commands[command.upper()](conn, word)
+        else:
+            raise ValueError
+    except ValueError:
+        conn.send('TRASNLATEERR"Invalid Command"'.encode())
+    except Exception as e:
+        print(e)
+    finally:
+        conn.close()
 
 
 def run():
@@ -113,12 +164,10 @@ def run():
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind((config['IP'], config['Port']))
         server.listen()
-        print('Server Started')
-        print(available_servers)
         while True:
             conn, addr = server.accept()
             print(addr[0] + " connected")
-            t = threading.Thread(target=client_thread, args=(conn, addr))
+            t = threading.Thread(target=client_thread, args=(conn, ))
             t.start()
     except OSError as e:
         print(e)
