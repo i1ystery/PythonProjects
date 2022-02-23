c027cc31a35abce3e27d80956b1bcccfa7d750b8
Max Kuzma <kuzmamax78@gmail.com>
2022-02-23 23:27:12 +0100

    Omega Authorization

--- Omega/Authorization.py
+++ Omega/Authorization.py
@@ -7,10 +7,13 @@ from ChallengeCodeGenerator import *
 
 APP_CLIENT_ID = '94b8cda0926111099fd079126ee497e7'
 APP_CLIENT_SECRET = '8ada8a9ff8547c2cb156a790af7741b93f074c8c6beaef5364047dbd3d82f5d5'
+CHALLENGE_CODE = CODE_VERIFIER = None
 
 
-def generate_authorization_link(challenge_code: str):
-    return f'https://https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id={APP_CLIENT_ID}&code_challenge={challenge_code}'
+def generate_authorization_link():
+    global CHALLENGE_CODE, CODE_VERIFIER
+    CHALLENGE_CODE = CODE_VERIFIER = get_new_code_verifier()
+    return f'https://https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id={APP_CLIENT_ID}&code_challenge={CHALLENGE_CODE}'
 
 
 def generate_new_user_token(auth_code: str, code_verifier: str) -> dict:
@@ -19,7 +22,7 @@ def generate_new_user_token(auth_code: str, code_verifier: str) -> dict:
         'app_client_id': APP_CLIENT_ID,
         'app_client_secret': APP_CLIENT_SECRET,
         'auth_code': auth_code,
-        'code_verifier': code_verifier,
+        'code_verifier': CODE_VERIFIER,
         'grant_type': 'authorization_code'
     }
     response = requests.post(url, data)
